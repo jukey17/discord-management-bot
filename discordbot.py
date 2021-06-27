1dae3a1d6733227da83c2bfcef51326858517997
jukey17 <keykun.jk827@gmail.com>
2021-06-27 16:53:10 +0900

    parse args

--- discordbot.py
+++ discordbot.py
@@ -1,9 +1,12 @@
 from discord.ext import commands
 import os
+import re
 import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+
+split_id_pattern = '(\d+)-(\d+)'
 
 
 @bot.event
@@ -17,8 +20,17 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+
 @bot.command()
 async def reaction_info(ctx, arg):
-    await ctx.send(arg)
+    if ctx.auther.bot:
+        return
+
+    result = re.match(split_id_pattern, arg)
+    if not result:
+        return
+
+    await ctx.send('channel={0}, message={1}'.format(result.group(1), result.group(2)))
+
 
 bot.run(token)
