c113b2f44cb63c8b7b3e8eb8965fc11902f1bbb3
jukey17 <jk.no.yuki@gmail.com>
2022-05-01 00:59:29 +0900

    refactoring

--- discordbot.py
+++ discordbot.py
@@ -9,8 +9,7 @@ import re
 import traceback
 
 import discord
-from discord import ChannelType, Intents, Guild
-from discord import User
+from discord import User, ChannelType, Intents, Guild
 from discord.abc import GuildChannel
 from discord.ext import commands
 
@@ -73,7 +72,7 @@ def parse_json(obj):
         str(obj)
 
 
-async def find_channel(guild, message_id):
+async def find_channel(guild: Guild, message_id: int):
     message = None
     result = None
     for channel in guild.channels:
@@ -98,7 +97,6 @@ async def count_messages(guild: Guild, channel_id: int, before: datetime.datetim
 
     if channel.type != ChannelType.text:
         raise TypeError(f'{channel.name} is not TextChannel: type={channel.type}')
-        return
 
     message_counters = [MessageCounter(member, channel) for member in guild.members if not member.bot]
 
@@ -130,7 +128,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def reaction_info(ctx, *args):
-    print(f'check author is bot: author={ctx.author}')
+    print(f'command executor={ctx.author}, time={datetime.datetime.now()}')
     if ctx.author.bot:
         await ctx.send('this is bot')
         return
@@ -140,165 +138,201 @@ async def reaction_info(ctx, *args):
         await ctx.send('command format is /reaction_info message={message_id}')
         return
 
-    parsed = parse_args(args)
+    async with ctx.typing():
+        parsed = parse_args(args)
 
-    if 'message' not in parsed:
-        await ctx.send('not found message_id')
-        return
+        if 'message' not in parsed:
+            await ctx.send('not found message_id')
+            return
 
-    message_id = int(parsed['message'])
-    print(f'fetch message: {message_id}')
-    channel, message = await find_channel(ctx.guild, message_id)
-    message = await channel.fetch_message(message_id)
+        message_id = int(parsed['message'])
+        print(f'fetch message: {message_id}')
+        channel, message = await find_channel(ctx.guild, message_id)
+        message = await channel.fetch_message(message_id)
 
-    if channel is None:
-        await ctx.send(f'not found channel, message_id={message_id}')
-        return
+        if channel is None:
+            await ctx.send(f'not found channel, message_id={message_id}')
+            return
 
-    if message is None:
-        await ctx.send(f'not found message, id={message_id}')
-        return
+        if message is None:
+            await ctx.send(f'not found message, id={message_id}')
+            return
 
-    if 'bot' in parsed:
-        members = copy.copy(ctx.guild.members)
-    else:
-        members = [member for member in ctx.guild.members if not member.bot]
-    no_reaction_members = copy.copy(members)
+        if 'bot' in parsed:
+            members = ctx.guild.members
+        else:
+            members = [member for member in ctx.guild.members if not member.bot]
+        no_reaction_members = copy.copy(members)
 
-    for reaction in message.reactions:
-        users = [user async for user in reaction.users()]
-        print(f'count reaction: {reaction}')
-        for member in members:
-            for user in users:
-                if member.id == user.id:
-                    if member in members:
-                        if member in no_reaction_members:
-                            no_reaction_members.remove(member)
+        for reaction in message.reactions:
+            users = [user async for user in reaction.users()]
+            print(f'count reaction: {reaction}')
+            for member in members:
+                if member not in no_reaction_members:
+                    continue
+                for user in users:
+                    if member.id == user.id:
+                        no_reaction_members.remove(member)
 
-    print('output result')
-    result = [f'"{channel.name} - {message.content}" reactions']
-    for reaction in message.reactions:
-        users = []
-        async for user in reaction.users():
-            users.append(user.name)
-        result.append(f'{reaction}: ' + ', '.join(users))
-    if len(no_reaction_members) > 0:
-        result.append('no reaction=' + ', '.join([member.name for member in no_reaction_members]))
+        print('output result')
+        result = [f'"{channel.name} - {message.content}" reactions']
+        for reaction in message.reactions:
+            users = []
+            async for user in reaction.users():
+                users.append(user.name)
+            result.append(f'{reaction}: ' + ', '.join(users))
+        if len(no_reaction_members) > 0:
+            result.append('no reaction=' + ', '.join([member.name for member in no_reaction_members]))
 
     await ctx.send('\n'.join(result))
 
 
 @bot.command()
 async def message_count(ctx, *args):
-    print(f'check author is bot: author={ctx.author}')
+    print(f'command executor={ctx.author}, time={datetime.datetime.now()}')
     if ctx.author.bot:
         await ctx.send('this is bot')
         return
 
     print(f'check arguments, {args}')
     if len(args) < 1:
-        await ctx.send('command format is /message_count channel={channel_id} '
-                       'options...(before={YYYY-mm-dd} after={YYYY-mm-dd})')
+        await ctx.send('format is /message_count channel={channel_id} before={YYYY-mm-dd} after={YYYY-mm-dd}')
         return
 
-    parsed = parse_args(args)
+    async with ctx.typing():
+        parsed = parse_args(args)
 
-    if 'channel' in parsed and 'channels' in parsed:
-        await ctx.send('cannot pass both channel and channels parameter to /message_count.')
-        return
-
-    if 'channel' not in parsed and 'channels' not in parsed:
-        await ctx.send('channel or channels parameter must be set.')
-        return
-
-    channel_ids = []
-    if 'channel' in parsed:
-        channel_ids.append(int(parsed['channel']))
-
-    if 'channels' in parsed:
-        channel_ids.extend([int(channel_id) for channel_id in parsed['channels'].split(',')])
-    print(f'parse channel_ids, {channel_ids}')
-
-    before = None
-    if 'before' in parsed:
-        before = datetime.datetime.strptime(parsed['before'], '%Y-%m-%d')
-    after = None
-    if 'after' in parsed:
-        after = datetime.datetime.strptime(parsed['after'], '%Y-%m-%d')
-
-    result_map = {}
-    for channel_id in channel_ids:
-        try:
-            channel, message_counters = await count_messages(ctx.guild, channel_id, before, after)
-        except Exception as e:
-            await ctx.send(e)
+        if 'channel' in parsed and 'channels' in parsed:
+            await ctx.send('cannot pass both channel and channels parameter to /message_count.')
             return
-        result_map[channel] = message_counters
-    results = convert_to_message_count_result(result_map)
 
-    filename = 'number_of_messages_in_channels.csv'
-    print(f'create {filename} buffer')
-    with contextlib.closing(io.StringIO()) as buffer:
-        fieldnames = ['user']
-        fieldnames.extend([key.name for key in result_map.keys()])
-        writer = csv.DictWriter(buffer, fieldnames)
-        writer.writeheader()
-        for user_id, result in results.items():
-            writer.writerow(result.to_dict())
+        if 'channel' not in parsed and 'channels' not in parsed:
+            await ctx.send('channel or channels parameter must be set.')
+            return
 
-        print(f'send {filename}')
-        buffer.seek(0)
-        await ctx.send(file=discord.File(buffer, filename))
+        channel_ids = []
+        try:
+            if 'channel' in parsed:
+                channel_ids.append(int(parsed['channel']))
+            if 'channels' in parsed:
+                channel_ids.extend([int(channel_id) for channel_id in parsed['channels'].split(',')])
+        except Exception as e:
+            print(e)
+            await ctx.send('can not parse channel_id/channel_ids.')
+            return
+
+        print(f'parse channel_ids, {channel_ids}')
+
+        before = None
+        after = None
+        try:
+            if 'before' in parsed:
+                before = datetime.datetime.strptime(parsed['before'], '%Y-%m-%d')
+            if 'after' in parsed:
+                after = datetime.datetime.strptime(parsed['after'], '%Y-%m-%d')
+        except Exception as e:
+            print(e)
+            await ctx.send('can not parse before/after.')
+            return
+
+        if after is not None and before is not None and after > before:
+            print(f'before={before}, after={after}')
+            await ctx.send('before must be a future than after.')
+            return
+
+        result_map = {}
+        for channel_id in channel_ids:
+            try:
+                channel, message_counters = await count_messages(ctx.guild, channel_id, before, after)
+            except Exception as e:
+                await ctx.send(f'cannot count messages: {e}')
+                return
+            result_map[channel] = message_counters
+        results = convert_to_message_count_result(result_map)
+
+        filename = 'number_of_messages_in_channels.csv'
+        print(f'create {filename} buffer')
+        with contextlib.closing(io.StringIO()) as buffer:
+            fieldnames = ['user']
+            fieldnames.extend([key.name for key in result_map.keys()])
+            writer = csv.DictWriter(buffer, fieldnames)
+            writer.writeheader()
+            for user_id, result in results.items():
+                writer.writerow(result.to_dict())
+            buffer.seek(0)
+
+            print(f'send {filename}')
+            await ctx.send(file=discord.File(buffer, filename))
 
 
 @bot.command()
 async def download_messages_json(ctx, *args):
-    print(f'check author is bot: author={ctx.author}')
+    print(f'command executor={ctx.author}, time={datetime.datetime.now()}')
     if ctx.author.bot:
         await ctx.send('this is bot')
         return
 
     print(f'check arguments, {args}')
     if len(args) < 1:
-        await ctx.send('command format is /message_count channel={channel_id} '
-                       'options...(before={YYYY-mm-dd} after={YYYY-mm-dd})')
+        await ctx.send('format is /download_messages_json channel={channel_id} before={YYYY-mm-dd} after={YYYY-mm-dd})')
         return
 
-    parsed = parse_args(args)
+    async with ctx.typing():
+        parsed = parse_args(args)
+        if 'channel' not in parsed:
+            await ctx.send('channel parameter must be set.')
+            return
 
-    if 'channel' not in parsed:
-        await ctx.send('not found channel_id')
-        return
+        try:
+            channel_id = int(parsed['channel'])
+        except Exception as e:
+            print(e)
+            await ctx.send('can not parse channel_id.')
+            return
 
-    channel_id = int(parsed['channel'])
-    print(f'fetch channel: {channel_id}')
-    channel = ctx.guild.get_channel(channel_id)
+        print(f'fetch channel: {channel_id}')
+        channel = ctx.guild.get_channel(channel_id)
 
-    if channel is None:
-        await ctx.send(f'not found channel, id={channel_id}')
-        return
+        if channel is None:
+            await ctx.send(f'not found channel, id={channel_id}')
+            return
 
-    if channel.type != ChannelType.text:
-        await ctx.send(f'not TextChannel, channel={channel.name}, type={channel.type}')
-        return
+        if channel.type != ChannelType.text:
+            await ctx.send(f'{channel.name} is not TextChannel: type={channel.type}')
+            return
 
-    before = datetime.datetime.strptime(parsed['before'], '%Y-%m-%d')
-    after = datetime.datetime.strptime(parsed['after'], '%Y-%m-%d')
-    print(f'read messages from history, after={after} before={before}')
-    outputs = []
-    async for message in channel.history(limit=None, before=before, after=after):
-        message_dict = dict(id=message.id, author=message.author.name, display_name=message.author.display_name,
-                            created_at=message.created_at, edited_at=message.edited_at, message=message.content)
-        outputs.append(message_dict)
+        before = None
+        after = None
+        try:
+            if 'before' in parsed:
+                before = datetime.datetime.strptime(parsed['before'], '%Y-%m-%d')
+            if 'after' in parsed:
+                after = datetime.datetime.strptime(parsed['after'], '%Y-%m-%d')
+        except Exception as e:
+            print(e)
+            await ctx.send('can not parse before/after.')
+            return
 
-    filename = 'messages.json'
-    print(f'create {filename} buffer')
-    with contextlib.closing(io.StringIO()) as buffer:
-        json.dump(outputs, buffer, default=parse_json, indent=2, ensure_ascii=False)
+        if after is not None and before is not None and after > before:
+            print(f'before={before}, after={after}')
+            await ctx.send('before must be a future than after.')
+            return
 
-        print(f'send {filename}')
-        buffer.seek(0)
-        await ctx.send(file=discord.File(buffer, filename))
+        print(f'read messages from history, after={after} before={before}')
+        outputs = []
+        async for message in channel.history(limit=None, before=before, after=after):
+            message_dict = dict(id=message.id, author=message.author.name, display_name=message.author.display_name,
+                                created_at=message.created_at, edited_at=message.edited_at, message=message.content)
+            outputs.append(message_dict)
+
+        filename = 'messages.json'
+        print(f'create {filename} buffer')
+        with contextlib.closing(io.StringIO()) as buffer:
+            json.dump(outputs, buffer, default=parse_json, indent=2, ensure_ascii=False)
+            buffer.seek(0)
+            print(f'send {filename}')
+            outputs = [f'#{channel.name}', after.strftime('%Y-%m-%d') + ' ~ ' + before.strftime('%Y-%m-%d')]
+            await ctx.send('\n'.join(outputs), file=discord.File(buffer, filename))
 
 
 bot.run(token)
