b0548dafb662c75980f275dad73504affc686ae5
jukey17 <jk.no.yuki@gmail.com>
2022-05-03 10:17:59 +0900

    fix heroku-relrease.sh

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,1 +1,11 @@
-echo "${GOOGLE_CREDENTIALS}" > /app/google-credentials.json
+#!/bin/bash
+
+FILE="/app/google-credentials.json"
+
+if [ ! -e $FILE ];then
+  echo "File not exists."
+fi
+echo "${GOOGLE_CREDENTIALS}" > $FILE
+if [ -e $FILE ]; then
+  echo "write ${FILE}"
+fi
