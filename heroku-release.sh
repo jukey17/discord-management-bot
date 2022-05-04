77f8bf9d8fb4c943d260bb919ea6ae8bd1586979
jukey17 <jk.no.yuki@gmail.com>
2022-05-04 16:29:39 +0900

    fix procfile

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 FILE="/app/google-credentials.json"
-
-if [ ! -e $FILE ];then
-  echo "File not exists."
-fi
 echo "${GOOGLE_CREDENTIALS}" > $FILE
-if [ -e $FILE ]; then
-  echo "write ${FILE}"
-fi
